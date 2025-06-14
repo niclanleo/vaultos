9a15d1b8c4c2cfe6e73113c8791ed517d749958c
arapachanate <arapachanate.@naoyoujituandeiMac.local>
2025-06-14 17:43:38 +0800

    自動更新 2025-06-14 17:43:38

--- sync.sh
+++ sync.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 echo " 準備同步專案到 GitHub..."
 git init
 git add .
-git commit -m " 初始上傳 VaultOS 專案架構"
+git commit -m "自動更新 $(date '+%Y-%m-%d %H:%M:%S')"
 git branch -M main
 git remote add origin https://github.com/niclanleo/vaultos.git
 git push -u origin main
