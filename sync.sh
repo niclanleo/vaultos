ef7609da7fab301799056bef902b1d8fc84ba1ce
arapachanate <arapachanate.@naoyoujituandeiMac.local>
2025-06-14 17:47:19 +0800

    自動更新 2025-06-14 17:47:19

--- sync.sh
+++ sync.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-cd "$(dirname "$0")"
 
-echo " 準備同步專案到 GitHub..."
-git init
 git add .
 git commit -m "自動更新 $(date '+%Y-%m-%d %H:%M:%S')"
-git branch -M main
-git remote add origin https://github.com/niclanleo/vaultos.git
-git push -u origin main
-
+git push origin main
 echo " 同步完成！請查看 https://github.com/niclanleo/vaultos"
